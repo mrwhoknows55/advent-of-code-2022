583f26b5d2be0b57fed7082a0f14695fb103830f
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-12-04 18:43:07 +0530

    add: day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String) = input.split("\n\n").map { elf ->
+        elf.lines().map { it.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun List<List<Int>>.topNElves(n: Int): Int = this.map { it.sum() }.sortedDescending().take(n).sum()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: String): Int = parseInput(input).topNElves(1)
+    fun part2(input: String): Int = parseInput(input).topNElves(3)
 
-    val input = readInput("Day01")
+    val input = File("src/day01_input.txt").readText()
     println(part1(input))
     println(part2(input))
 }
